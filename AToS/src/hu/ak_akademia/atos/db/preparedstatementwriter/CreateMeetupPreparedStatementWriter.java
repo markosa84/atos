2f944f84e1f1ed0fc6064a47cb9b5f8163f08a0b
Karda Milán <kardamilan@yahoo.hu>
2020-04-18 18:11:56 +0200

    Created new PreparedStatement

---
+++ AToS/src/hu/ak_akademia/atos/db/preparedstatementwriter/CreateMeetupPreparedStatementWriter.java
@@ -0,0 +1,33 @@
+package hu.ak_akademia.atos.db.preparedstatementwriter;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import hu.ak_akademia.atos.db.entity.Meetup;
+
+public class CreateMeetupPreparedStatementWriter implements PreparedStatementWriter<Meetup> {
+
+	private final Meetup meetup;
+
+	public CreateMeetupPreparedStatementWriter(Meetup meetup) {
+		this.meetup = meetup;
+	}
+
+	@Override
+	public void write(PreparedStatement preparedStatement) throws SQLException {
+		int i = 1;
+		preparedStatement.setLong(i++, meetup.getMeetupId());
+		preparedStatement.setString(i++, meetup.getUsername());
+		preparedStatement.setLong(i++, meetup.getInterestId());
+		preparedStatement.setString(i++, meetup.getName());
+		preparedStatement.setString(i++, meetup.getDescription());
+		preparedStatement.setBoolean(i++, meetup.getOnline());
+		preparedStatement.setLong(i++, meetup.getCityId());
+		preparedStatement.setString(i++, meetup.getLocation());
+		preparedStatement.setTimestamp(i++, Timestamp.valueOf(meetup.getDateAndTime()));
+		preparedStatement.setDouble(i++, meetup.getDuration());
+		preparedStatement.setInt(i++, meetup.getParticipantLimit());
+	}
+
+}
---
+++ AToS/src/hu/ak_akademia/atos/db/preparedstatementwriter/CreateUserInterestMapPreparedStatementWriter.java
@@ -0,0 +1,24 @@
+package hu.ak_akademia.atos.db.preparedstatementwriter;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import hu.ak_akademia.atos.db.entity.UserInterestMap;
+
+public class CreateUserInterestMapPreparedStatementWriter implements PreparedStatementWriter<UserInterestMap> {
+
+	private final UserInterestMap userInterestMap;
+
+	public CreateUserInterestMapPreparedStatementWriter(UserInterestMap userInterestMap) {
+		this.userInterestMap = userInterestMap;
+	}
+
+	@Override
+	public void write(PreparedStatement preparedStatement) throws SQLException {
+		int i = 1;
+		preparedStatement.setLong(i++, userInterestMap.getUserInterestMapId());
+		preparedStatement.setString(i++, userInterestMap.getUsername());
+		preparedStatement.setLong(i++, userInterestMap.getInterestId());
+	}
+
+}
